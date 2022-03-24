c6af697a4e55be54e86d9dfc991352b37d782868
karppienkingi <mirka.vaananen@gmail.com>
2022-03-24 13:27:15 +0200

    JUnit tests for Maksukortti

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -10,11 +10,54 @@ public class MaksukorttiTest {
 
     @Before
     public void setUp() {
-        kortti = new Maksukortti(10);
+        kortti = new Maksukortti(1000);
     }
 
     @Test
     public void luotuKorttiOlemassa() {
-        assertTrue(kortti!=null);      
+        assertTrue(kortti != null);
+    }
+    
+    @Test
+    public void saldoPalauttaaOikein() {
+        assertEquals(1000, kortti.saldo());
+    }
+
+    @Test
+    public void saldoOnAlussaOikein() {
+        assertEquals("saldo: 10.0", kortti.toString());
+    }
+
+    @Test
+    public void lataaRahaalataaOikein() {
+        kortti.lataaRahaa(100);
+        assertEquals("saldo: 11.0", kortti.toString());
+    }
+
+    @Test
+    public void otaRahaaVahentaaOikein() {
+
+        kortti.otaRahaa(50);
+        assertEquals("saldo: 9.50", kortti.toString());
+    }
+
+    @Test
+    public void otaRahaaEiVahennaTurhaan() {
+        kortti.otaRahaa(1100);
+        assertEquals("saldo: 10.0", kortti.toString());
+    }
+
+    @Test
+    public void otaRahaaPalauttaaTrue() {
+        Boolean tosi = kortti.otaRahaa(50);
+        assertTrue(tosi);
+
+    }
+
+    @Test
+    public void otaRahaaPalauttaaFalse() {
+        
+        Boolean epätosi = kortti.otaRahaa(1100);
+        assertFalse(epätosi);
     }
 }
