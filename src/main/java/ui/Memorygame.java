2050ec417cc109dc802d5fa02f95fd1dc5b8408b
karppienkingi <mirka.vaananen@gmail.com>
2022-04-13 21:27:17 +0300

    added new difficultylevels

--- src/main/java/ui/Memorygame.java
+++ src/main/java/ui/Memorygame.java
@@ -10,6 +10,7 @@ import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.stage.Stage;
 
 /**
  *
@@ -18,28 +19,39 @@ import javafx.scene.text.Font;
 public class Memorygame {
 
     private Gamelogic logic;
-    private int amountOfPairs;
+    private final int amountOfPairs;
     private Button[] buttons;
     private Button newgame;
-    private Scene scene;
+    private Button toMenu;
+    private Scene gameView;
     private VBox game;
     private Label label;
+    private Scene menuView;
+    private Stage stage;
 
-    public Memorygame(int amount) {
+    public Memorygame(int amount, Scene scene, Stage stage) {
+        this.stage = stage;
+        this.menuView = scene;
         this.amountOfPairs = amount;
-        this.logic = new Gamelogic(3);
+        this.logic = new Gamelogic(this.amountOfPairs);
 
-        buttons = new Button[6];
+        buttons = new Button[this.amountOfPairs * 2];
+
         this.newgame = new Button("new game");
         this.newgame.setFocusTraversable(false);
-        this.newgame.setMinSize(20,40);
+        this.newgame.setMinSize(20, 40);
         this.newgame.setFont(Font.font("Stone", 16));
+
+        this.toMenu = new Button("Back to menu");
+        this.toMenu.setFocusTraversable(false);
+        this.toMenu.setMinSize(20, 40);
+        this.toMenu.setFont(Font.font("Stone", 16));
 
         this.label = new Label("");
         this.label.setFont(Font.font("Stone", 40));
         this.label.setText("");
 
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < amount * 2; i++) {
             buttons[i] = new Button("" + (i));
             buttons[i].setMinSize(100, 200);
             buttons[i].setFocusTraversable(false);
@@ -76,28 +88,41 @@ public class Memorygame {
         this.game.getChildren().add(board);
         this.game.setFillWidth(false);
         this.game.getChildren().add(this.label);
-        this.game.getChildren().add(newgame);
+        this.game.getChildren().addAll(this.newgame, this.toMenu);
         this.newgame.setVisible(false);
+        this.toMenu.setVisible(false);
 
-        this.scene = new Scene(this.game, 600, 600, Color.GHOSTWHITE);
+        this.gameView = new Scene(this.game, 600, 600, Color.GHOSTWHITE);
     }
 
     public Scene setGame() {
-        return scene;
+        return gameView;
+    }
+
+    public void setMenu() {
+        this.stage.setScene(menuView);
     }
 
     private void endGame() {
         this.newgame.setVisible(true);
+        this.toMenu.setVisible(true);
         this.newgame.setOnAction((event) -> {
-            this.logic = new Gamelogic(3);
+            this.logic = new Gamelogic(this.amountOfPairs);
             this.newgame.setVisible(false);
+            this.toMenu.setVisible(false);
             for (Button button : buttons) {
                 button.setDisable(false);
                 this.label.setText("");
             }
+            setGame();
         });
-        
-        setGame();
+
+        this.toMenu.setOnAction((event) -> {
+            this.newgame.setVisible(false);
+            this.toMenu.setVisible(false);
+            setMenu();
+        });
+
     }
 
 }
