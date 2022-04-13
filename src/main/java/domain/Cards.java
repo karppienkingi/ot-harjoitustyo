207473155eee91a2cce2c131da721021cef0a4d4
karppienkingi <mirka.vaananen@gmail.com>
2022-04-13 17:32:38 +0300

    new project folder, no changes to code

---
+++ src/main/java/domain/Cards.java
@@ -0,0 +1,45 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+/**
+ *
+ * @author Mirka
+ */
+public class Cards {
+
+    private Images images;
+    int numberOfCard;
+    private final ArrayList<Integer> cards;
+    private final HashMap<Integer, Integer> pairs;
+
+    public Cards(int amountOfPairs) {
+        this.cards = new ArrayList<>();
+        this.pairs = new HashMap<>();
+        for (int i = 0; i < amountOfPairs * 2; i++) {
+            this.cards.add(i);
+        }
+        //this.images = new Images(amountOfPairs);
+        createPairs();
+    }
+
+    private void createPairs() {
+        Collections.shuffle(this.cards);
+
+        for (int i = 0; i < 6; i += 2) {
+            pairs.put(this.cards.get(i), this.cards.get(i + 1));
+            pairs.put(this.cards.get(i + 1), this.cards.get(i));
+        }
+    }
+    
+    public HashMap<Integer, Integer> getPairs() {
+        return this.pairs;
+    }
+
+    public int getPair(int cardnro) {
+        return this.pairs.get(cardnro);
+    }
+
+}
---
+++ src/test/java/domain/CardsTest.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package domain;
+
+import java.util.HashMap;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author Mirka
+ */
+public class CardsTest {
+    
+    public CardsTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    /**
+     * Test of getPairs method, of class Cards.
+     */
+
+    
+}
