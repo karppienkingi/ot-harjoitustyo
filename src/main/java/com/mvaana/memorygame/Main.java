3d41502ca64da33764c8a1ce66fe5c7228382edb
karppienkingi <mirka.vaananen@gmail.com>
2022-05-13 15:19:04 +0300

    move Main class

---
+++ src/main/java/com/mvaana/memorygame/Main.java
@@ -0,0 +1,17 @@
+package com.mvaana.memorygame;
+
+import com.mvaana.memorygame.ui.App;
+/**
+ *
+ * @author Mirka
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        App.main(args);
+    }
+    
+}
